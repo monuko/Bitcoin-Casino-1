262261c77dd2bfa226b05223d02b3073ad073d77
Aman Singh <besoeasy@users.noreply.github.com>
2019-01-16 23:59:05 +0530

    Update index.php

--- index.php
+++ index.php
@@ -79,6 +79,13 @@
 	<script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 	<script>  
+		
+		  $(document).ready(function(){
+    $('.collapsible').collapsible();
+  });
+		
+		
+		
 var slider = document.getElementById("myRange");
 var output = document.getElementById("demo");
 var output2 = document.getElementById("demo2");
