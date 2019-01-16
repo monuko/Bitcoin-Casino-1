3f001f40f89db1f421a3c04b495929ee664318e4
Aman Singh <besoeasy@users.noreply.github.com>
2019-01-17 04:18:38 +0530

    Update x.php

--- x.php
+++ x.php
@@ -19,4 +19,63 @@ echo "electrum payto " .$obj2['sender']. " ". $obj2['winamount'] ."  | electrum 
 
 }
 
+
+
+function sendMessage() {
+    $content      = array(
+        "en" => 'English Message'
+    );
+    $hashes_array = array();
+    array_push($hashes_array, array(
+        "id" => "like-button",
+        "text" => "Like",
+        "icon" => "http://i.imgur.com/N8SN8ZS.png",
+        "url" => "https://yoursite.com"
+    ));
+    array_push($hashes_array, array(
+        "id" => "like-button-2",
+        "text" => "Like2",
+        "icon" => "http://i.imgur.com/N8SN8ZS.png",
+        "url" => "https://yoursite.com"
+    ));
+    $fields = array(
+        'app_id' => "e73f244e-19a3-4aa1-9c17-7c3de9f34b72",
+        'included_segments' => array(
+            'All'
+        ),
+        'data' => array(
+            "foo" => "bar"
+        ),
+        'contents' => $content,
+        'web_buttons' => $hashes_array
+    );
+    
+    $fields = json_encode($fields);
+    print("\nJSON sent:\n");
+    print($fields);
+    
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, "https://onesignal.com/api/v1/notifications");
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        'Content-Type: application/json; charset=utf-8',
+        'Authorization: Basic NmY2NjY1MGItYjYyNS00OGU1LThhMjYtNDRjOTFiYWU5OTlh'
+    ));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    curl_setopt($ch, CURLOPT_HEADER, FALSE);
+    curl_setopt($ch, CURLOPT_POST, TRUE);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+    
+    $response = curl_exec($ch);
+    curl_close($ch);
+    
+    return $response;
+}
+
+$response = sendMessage();
+$return["allresponses"] = $response;
+$return = json_encode($return);
+$data = json_decode($response, true);
+$id = $data['id'];
+
 ?> 
