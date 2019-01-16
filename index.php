7891603ef5da87622e2274da56b7aac181726ca0
Aman Singh <besoeasy@users.noreply.github.com>
2019-01-16 21:02:09 +0530

    Update index.php

--- index.php
+++ index.php
@@ -12,18 +12,16 @@
     <body>
 
 
-     <div class="container">	
- <div class="navbar-fixed">
-    <nav>
-      <div class="nav-wrapper">
-        <a href="#!" class="brand-logo">SKOBET</a>
-        <ul class="right hide-on-med-and-down">
-          <li><a href="sass.html">Sass</a></li>
-          <li><a href="badges.html">Components</a></li>
-        </ul>
-      </div>
-    </nav>
-  </div>  </div>
+   <div class="container"><div class="row"><nav>
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo"><i class="material-icons">dehaze</i> SKOBET</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="https://pastebin.com/raw/QmeqNjNE">Fairness</a></li>
+        <li><a href="https://bitcointalk.org/index.php?topic=5073341.0">Bitcointalk</a></li>
+        <li><a href="https://t.me/btcjua">Telegram</a></li>
+      </ul>
+    </div>
+  </nav></div> </div>
         	
 			
 
