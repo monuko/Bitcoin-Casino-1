9390b8ec323728ae0b576eee40c307ecc497c628
Aman Singh <besoeasy@users.noreply.github.com>
2019-01-17 00:00:28 +0530

    Update index.php

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
      <div class="container">
    <div class="row">
 	   
-	     <ul class="collapsible">
+  <ul class="collapsible popout">
     <li>
       <div class="collapsible-header"><i class="material-icons">filter_drama</i>First</div>
       <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
