7686b0332d1255d9cedc1ff24844799334098d6f
Aman Singh <besoeasy@users.noreply.github.com>
2019-01-17 00:37:50 +0530

    Update index.php

--- index.php
+++ index.php
@@ -48,7 +48,7 @@
     </li>
     <li>
       <div class="collapsible-header"><i class="material-icons">blur_on</i>FAQ Payouts </div>
-      <div class="collapsible-body"><span>When Your Transaction has 5+ Confirmation on blockchain, Payout is Automatically gets sent to you.  
+      <div class="collapsible-body"><span>When Your Transaction has 5+ Confirmation on blockchain, Payout is Automatically sent to you. 
 	    Its Sent To Same Address From Which You Send, if multiple inputs are used then winnigs are sent to the 1st Input.
 	     </span></div>
     </li>
