12472d5a12982511ddb810bd7c240edef79da706
Aman Singh <besoeasy@users.noreply.github.com>
2019-01-19 03:17:23 +0530

    Update x.php

--- x.php
+++ x.php
@@ -1,19 +1,23 @@
 <?php
 $scrape = file_get_contents("https://www.blockchain.com/btc/address/xpub661MyMwAqRbcEaiJEUfroC7RbmFjuJMWsTSj49Tv4uSEhcddGcWVyBKKQKCu4ZKHwFKYff66HdoJ3u97RF1yCAWAnGdtHsGFJYMVsfP6ypA?filter=1#"); 
+sleep(5);
 $obj = json_decode(file_get_contents('http://api.smartbit.com.au/v1/blockchain/address/xpub661MyMwAqRbcEaiJEUfroC7RbmFjuJMWsTSj49Tv4uSEhcddGcWVyBKKQKCu4ZKHwFKYff66HdoJ3u97RF1yCAWAnGdtHsGFJYMVsfP6ypA?dir=asc&limit=111'), true);
+sleep(5);
+
 
 $matches = array();
 preg_match('/[a-fA-F0-9]{64}/', $scrape, $matches);
 $blockobj = json_decode(file_get_contents('https://api.smartbit.com.au/v1/blockchain/tx/' . $matches[0]), true);
 $lowblock = $blockobj['transaction']['first_seen'];
+sleep(5);
+
 
 
 foreach ($obj['address']['transactions'] as $t) {
 
 if($t['first_seen'] > $lowblock){  
 $obj2 = json_decode(file_get_contents('https://skobet.herokuapp.com/api.php?t=' .$t['txid']), true);
- 
-echo $obj2['sender'];  
+  
 if($obj2['result']>0){  
 $response = sendMessage($obj2['sender'],$obj2['winamount']);  
 echo "electrum payto " .$obj2['sender']. " ". $obj2['winamount'] ."  | electrum broadcast - " ;
