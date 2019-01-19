717aaabd419fd0162b51b47bdbf6b1bf7c1cfaf7
Aman Singh <besoeasy@users.noreply.github.com>
2019-01-20 03:38:00 +0530

    Update index.php

--- index.php
+++ index.php
@@ -51,10 +51,6 @@
         	
 			
 	    
-
-     <div class="container">
-   <div class="row">
-
 <div class="col s12 l6">	   
 
   <ul class="collapsible popout">
@@ -97,12 +93,12 @@
 <div class="progress"><div class="indeterminate"></div></div>
 </div>
 
- </div></div>
 
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-	<script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 <script>  
 //ui script		
 $(document).ready(function(){
