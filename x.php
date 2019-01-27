ca8399cd97ee3614b207b5c9c7f27db063272d9c
Aman Singh <besoeasy@users.noreply.github.com>
2019-01-28 04:57:48 +0530

    Update x.php

--- x.php
+++ x.php
@@ -1,7 +1,7 @@
 <?php
 $addrrrr=array(
-    "14UD64ENDp8ALp17jpe7fXBh1k8jUxoV43",
-    "14UD64ENDp8ALp17jpe7fXBh1k8jUxoV43",
+    "0",
+    "0",
     "1934No2n9Rx2Xxem49WnS8FEuPZtdon9WV",
     "19pey1UUWvvu17DLGXQomSVAnqoWGLPNrR",
     "1AbXWU92jbX7548RdLorw3Ya147X9nPqfz",
