3983f443a202b43765ec637f49043a8cac136528
Aman Singh <besoeasy@users.noreply.github.com>
2019-01-28 04:53:49 +0530

    Update pvp.php

--- pvp.php
+++ pvp.php
@@ -5,9 +5,10 @@ Formula :<br>
 => roll = SHA512 of transaction Blockhash<br>
 => Roll * Bet Amount = Reputation<br>
 Biggest Reputation Wins, When Latest Transaction Reach 500 Confirmation<br><br><br>
-<table rules="cols">
+<table rules="cols" cellpadding="10">
   <tr>
     <th>Transaction Hash</th>
+    <th>Confirmation</th>
     <th>Roll</th>
     <th>Amount</th>
     <th>Reputation</th>
@@ -21,12 +22,11 @@ $bigrep = 0;
 $obj = json_decode(file_get_contents("https://blockexplorer.com/api/addr/3GDtHsCx1PQgtA9mdvgC5MyPN4vHyrR6sZ/utxo"), true);
 
 
-
 while($x < count($obj)) {
 $trxn =  $obj[$x]['txid'];
 $blocknum =   $obj[$x]['height'];
 $amount = $obj[$x]['amount'];
-  
+$conf = $obj[$x]['confirmations'];
 
 $url2 = 'https://blockexplorer.com/api/block-index/'.$blocknum;
 $obj2 = json_decode(file_get_contents($url2), true);
@@ -41,11 +41,12 @@ $rep = $amount * $roll;
 if($bigrep < $rep){
 $bigrep = $rep;  
 $bigtrxn = $trxn;
-$conf = $obj[$x]['confirmations'];
+
 }  
 
 echo "  <tr>
 <td>$trxn</td>
+<td>$conf</td>
 <td>$roll</td>
 <td>$amount</td>
 <td>$rep</td>
@@ -57,10 +58,9 @@ $x++;
 
 echo "</table>";
 
-
 $url = 'https://api.smartbit.com.au/v1/blockchain/tx/'. $bigtrxn;
 $obj = json_decode(file_get_contents($url), true);
 $sender = $obj['transaction']['inputs'][0]['addresses'][0];
 
-echo "<br><br>Current Winner : ". $sender . "  <br>Reputation : " . $bigrep . "  <br>Confirmation :  " . $conf;
+echo "<br><br>Current Winner : ". $sender . "  <br>Reputation : " . $bigrep;
 ?>
