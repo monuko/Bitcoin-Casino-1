c4ffa72ec92da0b42bedcbaf48331991b7159518
Aman Singh <besoeasy@users.noreply.github.com>
2019-02-01 03:29:06 +0530

    Update index.php

--- index.php
+++ index.php
@@ -19,19 +19,21 @@
 
 
 
-<div class="container">
-    <div class="row">
-        <nav>
-            <div class="nav-wrapper">
-                <a href="" onClick="window.location.href=window.location.href" class="brand-logo"><i class="material-icons">dehaze</i> SKOBET</a>
-                <ul id="nav-mobile" class="right hide-on-med-and-down">
-                    <li><a target="_blank" href="https://pastebin.com/raw/x2y3iZQd">Fairness</a></li>
-                    <li><a target="_blank" href="https://www.blockchain.com/btc/xpub/xpub661MyMwAqRbcEiNk4KqoCzFJEfqDKkoyYZ1sEkJu3G4EKJVNczpNmUq9AYrdV8RJ4Swcd3N3CtBAZwat1RrqaQycG5wkZaVRCWQC5m6UiTL">View Skobet Wallet</a></li>
-                </ul>
-            </div>
-        </nav>
+<div class="container"><div class="row">
+	 <div class="navbar-fixed">
+  <nav>
+    <div class="nav-wrapper">
+      <a href="#" onClick="window.location.href=window.location.href"  class="brand-logo"><i class="material-icons">dehaze</i> SKOBET</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="sass.html">Sass</a></li>
+
+      </ul>
     </div>
-</div>
+		 </nav>    </div></div></div>
+
+
+
+
 
 <div class="container">
     <div class="row">
