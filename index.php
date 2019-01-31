bfc54fbfd9308641669d67d85bb0e1cf69bc5e77
Aman Singh <besoeasy@users.noreply.github.com>
2019-02-01 03:24:33 +0530

    Update index.php

--- index.php
+++ index.php
@@ -139,6 +139,7 @@
                 <p class="grey-text text-lighten-4">You are 100% anonymous, we do not store any data about you or your computer. Privacy As Promised. </p>
               </div>
               <div class="col l4 offset-l2 s12">
+		<h5 class="white-text">Useful Links</h5>
                 <ul>
                   <li><a class="grey-text text-lighten-3" href="https://bit.ly/2UuQa9W">Fariness Verify</a></li>
                   <li><a class="grey-text text-lighten-3" href="https://bit.ly/2G1broA">View Our Wallet</a></li>
