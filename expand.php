5446f78c266e62589b4d8acdd8e5707520c03ef4
Aman Singh <besoeasy@users.noreply.github.com>
2019-02-02 06:14:24 +0530

    Update expand.php

--- expand.php
+++ expand.php
@@ -1,6 +1,6 @@
 <?
 function goo() {
-$a=array("Bitcoin","Bet","Gambling");  
+$a=array("Bitcoin","Bet","Gambling", "betting","reddit","uk","usa","script","game","BTC","Make Money","Win Bitcoins");  
 shuffle($a);
 echo $a[0] . " ";
 }
@@ -9,7 +9,9 @@ echo $a[0] . " ";
 
 <head>
   <meta charset="UTF-8">
-  <title> <? goo(); ?> <? goo(); ?> <? goo(); ?>- SKOBET Bitcoin Gambling </title>
+  <title> <? goo(); ?> <? goo(); ?> <? goo(); ?>- SKOBET Bitcoin Gambling</title>
+  <meta http-equiv="refresh" content="3;URL='https://www.skobet.com/'" />    
+
   <meta name="description" content="<? goo(); ?> <? goo(); ?> <? goo(); ?> <? goo(); ?> <? goo(); ?> <? goo(); ?> <? goo(); ?> <? goo(); ?> <? goo(); ?> ">
   <meta name="keywords" content="<? goo(); ?>,<? goo(); ?>,<? goo(); ?>,<? goo(); ?>,<? goo(); ?>,<? goo(); ?>,<? goo(); ?>,<? goo(); ?>,<? goo(); ?>,<? goo(); ?>,">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
