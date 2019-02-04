eb661cd0850b1a6231d647a07679828ddcd81087
Aman Singh <besoeasy@users.noreply.github.com>
2019-02-05 04:39:51 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,9 @@
 <meta name="Description" content="Bitcoin Blockchain Betting, Earn Upto 20 Times Your Bet.  SKOBET is Decenterlized, Fast, Fair, Provides Each Bet Verification Directly on Bitcoin Blockchain. ">
 <meta name="Keywords" content="Bitcoin Bet,SKOBET,skobet bitcoin,skobet btc,skobet play,skobet fair">
 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-<meta http-equiv="refresh" content="300">	    
+<meta http-equiv="refresh" content="300">	
+	    <meta name="google-site-verification" content="EnlUraU4X2LSBU4Z09CkyhhHhF6Udlx98OHCucIDQzE" />
+
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 </head>
