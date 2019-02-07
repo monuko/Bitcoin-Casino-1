cc829d43cd6aca2ede8a5d8143a014c724cf4800
Aman Singh <besoeasy@users.noreply.github.com>
2019-02-08 02:16:36 +0530

    Update index.php

--- index.php
+++ index.php
@@ -58,14 +58,16 @@
             <div class="row">
               <div class="col l6 s12">
                 <h5 class="white-text">Terms Of Service</h5>
-                <p class="grey-text text-lighten-4">You are 100% anonymous, we do not store any data about you or your computer. Privacy As Promised. </p>
+                <p class="grey-text text-lighten-4">
+		      You are 100% anonymous, Skobet Doesn't Store any Of Your Data. Privacy As Promised. House Edge: 1%
+		     </p>
               </div>
               <div class="col l4 offset-l2 s12">
-		<h5 class="white-text">Useful Links</h5>
+		<h5 class="white-text">Know More</h5>
                 <ul>
                   <li><a class="grey-text text-lighten-3" target="_blank" href="https://bit.ly/2UuQa9W">Fariness Verify</a></li>
                   <li><a class="grey-text text-lighten-3" target="_blank" href="https://bit.ly/2G1broA">View Our Wallet</a></li>
-                  <li><a class="grey-text text-lighten-3" target="_blank" href="https://bit.ly/2HI9hMq">Telegram Group</a></li>
+                  <li><a class="grey-text text-lighten-3" target="_blank" href="https://bitcointalk.org/index.php?topic=5098423">BitcoinTalk</a></li>
                 </ul>
               </div>
             </div>
