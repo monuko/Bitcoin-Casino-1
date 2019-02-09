0f4cb8cd8713c03df62ffe48b2965996ba181e72
Aman Singh <besoeasy@users.noreply.github.com>
2019-02-09 16:15:19 +0530

    Update index.php

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
         <div class="container"> <div class="row">
         <div class="col s12 l6">
 
-		      <div class="red lighten-2">
+		      <div class=" light-blue lighten-4">
                      <blockquote><span id="maxbet">Deposit Bitcoin (BTC) Only, 1 Confirmation Required. Deposit From Your Wallet Only, Payouts Are Sent Back To Same Address.</span></blockquote>
                        <blockquote> <img  id="addrimg" alt="Deposit Bitcoin only" src="https://chart.googleapis.com/chart?chs=200x200&cht=qr&chld=L|1&chl=bitcoin:1V7L2QKLuZ1m7PNFfHAcR2ddnwoZBqhTk" /></blockquote>
                          <blockquote><input id="addrvalue" type="text" class="form-control" value="1V7L2QKLuZ1m7PNFfHAcR2ddnwoZBqhTk"> </blockquote>
