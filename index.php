fe16f1759a34ed38d566ce3008024d565839defd
Aman Singh <besoeasy@users.noreply.github.com>
2019-02-09 16:16:27 +0530

    Update index.php

--- index.php
+++ index.php
@@ -22,15 +22,13 @@
 
         <div class="container"> <div class="row">
         <div class="col s12 l6">
-
-		      <div class=" light-blue lighten-4">
                      <blockquote><span id="maxbet">Deposit Bitcoin (BTC) Only, 1 Confirmation Required. Deposit From Your Wallet Only, Payouts Are Sent Back To Same Address.</span></blockquote>
                        <blockquote> <img  class="center" id="addrimg" alt="Deposit Bitcoin only" src="https://chart.googleapis.com/chart?chs=200x200&cht=qr&chld=L|1&chl=bitcoin:1V7L2QKLuZ1m7PNFfHAcR2ddnwoZBqhTk" /></blockquote>
                          <blockquote><input id="addrvalue" type="text" class="form-control" value="1V7L2QKLuZ1m7PNFfHAcR2ddnwoZBqhTk"> </blockquote>
                       <blockquote>   <p class="range-field">
                         <input type="range" id="myRange" min="2" max="20" class="tooltipped" data-position="bottom" data-tooltip="Set Your Multiplier" /> </p> </blockquote>
                         <blockquote> Multiplier : <span id="demo">20X</span> - Winchance : <span id="demo2">4.950%</span> </blockquote>
-		</div>
+
         <div class="progress"> <div class="indeterminate"></div></div>
 	<div id="myplot"></div>
         </div>
