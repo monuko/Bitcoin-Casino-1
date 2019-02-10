585c32f5230dfc347066056ee662c30297a32723
Aman Singh <besoeasy@users.noreply.github.com>
2019-02-10 17:41:32 +0000

    Update index.php

--- index.php
+++ index.php
@@ -22,21 +22,17 @@
         <div class="container"> <div class="row">
 
 
-
-		<div class="s12 l6">
-
 		<div class="col s12 l4">
                 <img id="addrimg" alt="Deposit Bitcoin only" src="https://chart.googleapis.com/chart?chs=200x200&cht=qr&chld=L|1&chl=bitcoin:1V7L2QKLuZ1m7PNFfHAcR2ddnwoZBqhTk" />
 		</div>
 
 		<div class="col s12 l8">
                 <span id="addrvalue">1V7L2QKLuZ1m7PNFfHAcR2ddnwoZBqhTk</span><br>
-                <p class="range-field"><input type="range" id="myRange" min="2" max="20" class="tooltipped" data-position="bottom" data-tooltip="Set Your Multiplier" /> </p> <br>
+                <input type="range" id="myRange" min="2" max="20" class="tooltipped" data-position="bottom" data-tooltip="Set Your Multiplier" /><br>
         	 Multiplier : <span id="demo">20X</span> - Winchance : <span id="demo2">4.950%</span> <br>
 		<span id="maxbet">Maxbet 0.2 BTC</span>
 		</div>
 
-                </div>
 
 <div class="col s12 l12">
 <div id="myplot"></div>
