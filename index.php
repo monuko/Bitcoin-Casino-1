0ed915637b71a0968d8f0cb24610bfb2f8448643
Aman Singh <besoeasy@users.noreply.github.com>
2019-02-12 02:34:00 +0530

    Update index.php

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
 <div class="col s12 l12">
 <center>
 <img style="max-width:640px;" width=100% height=auto src="https://i.ibb.co/wcnqcxz/skobet.png"/><br>
-<img id="addrimg" alt="Deposit Bitcoin only" src="https://chart.googleapis.com/chart?chs=250x250&cht=qr&chld=L|1&chl=bitcoin:1V7L2QKLuZ1m7PNFfHAcR2ddnwoZBqhTk" />
+<img id="addrimg" alt="Deposit Bitcoin only" src="" />
 <br>
 <span id="addrvalue">1V7L2QKLuZ1m7PNFfHAcR2ddnwoZBqhTk</span>
 </center>
