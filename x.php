92a14b0506c1ce95d4d8613db492edc75e37087d
Aman Singh <besoeasy@users.noreply.github.com>
2019-02-24 18:38:40 +0530

    Update x.php

--- x.php
+++ x.php
@@ -3,7 +3,7 @@
 
 <head>
   <meta charset="UTF-8">
-  <title>Sticky Navigation Menu With Smooth Scrolling</title>
+  <title>Skobet </title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
   <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css'>
