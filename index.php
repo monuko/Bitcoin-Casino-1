6c041e949203dbb244c05740b0600447a11674e0
Aman Singh <besoeasy@users.noreply.github.com>
2019-02-27 20:20:43 +0000

    Update index.php

--- index.php
+++ index.php
@@ -59,7 +59,20 @@ Bet Winchance : <span id="demo2">4.950 %</span></blockquote></center>
           
 <div class="col s12 l12">
 <div class="responsive-table">
-<table class="responsive-table" id="myTable"></table>
+<table class="responsive-table" id="myTable">
+  <thead>
+          <tr>
+              <th>Sender</th>
+              <th>Item Name</th>
+              <th>Item Price</th>
+              <th>Name</th>
+              <th>Item Name</th>
+              <th>Item Price</th>
+              <th>TRXN</th>
+          </tr>
+        </thead>
+
+</table>
 </div>
 </div>
 
