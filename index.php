63988cc97c33dd42a1f0966c54fb194bf2512b38
Aman Singh <besoeasy@users.noreply.github.com>
2019-03-06 00:36:16 +0400

    Update index.php

--- index.php
+++ index.php
@@ -9,6 +9,48 @@
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 <script src="https://wchat.freshchat.com/js/widget.js"></script>
+
+
+<style>
+.slidecontainer {
+  width: 100%;
+}
+
+.slider {
+  -webkit-appearance: none;
+  width: 100%;
+  height: 25px;
+  background: #d3d3d3;
+  outline: none;
+  opacity: 0.7;
+  -webkit-transition: .2s;
+  transition: opacity .2s;
+}
+
+.slider:hover {
+  opacity: 1;
+}
+
+.slider::-webkit-slider-thumb {
+  -webkit-appearance: none;
+  appearance: none;
+  width: 25px;
+  height: 25px;
+  background: #4CAF50;
+  cursor: pointer;
+}
+
+.slider::-moz-range-thumb {
+  width: 25px;
+  height: 25px;
+  background: #4CAF50;
+  cursor: pointer;
+}
+</style>
+
+
+
+
 </head>
 <body>
 
@@ -32,7 +74,11 @@
 </div>
 
 <div class="col s12 l12">
-<p class="range-field"><input type="range" id="myRange" min="2" max="40" /></p>
+
+<div class="slidecontainer">
+  <input type="range" min="2" max="40" value="4" class="slider" id="myRange">
+</div>
+
 </div>
 
 
