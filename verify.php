e45c5ae7b4ecb0a8ee16e7c0885adb9b57db7a0c
Aman Singh <besoeasy@users.noreply.github.com>
2019-03-06 08:51:43 +0300

    Update verify.php

--- verify.php
+++ verify.php
@@ -6,8 +6,25 @@ $url = 'https://skobet.com/a.php?t='. $trxn;
 $obj = json_decode(file_get_contents($url), true);
 
 
-echo " Formula is: hash('sha512', hash('sha512', $trxn) . hash('sha512', $blockhash) . hash('sha512', $blocknum) ) " . "<br>";
+echo "Sender : " . $obj[sender] . "<br>";
+echo "Transaction Hash : " . $obj[trxn] . "<br>";
+echo "Confirmed In : " . $obj[blocknum] . "<br>";
+echo "Block " . $obj[blocknum] . " Hash is " . $obj[blockhash] . "<br>";
+echo  "<br>";
 
+echo  "[FORMULA] A = sha512(transaction hash) + sha512(blockhash) + sha512(blocknum)" . "<br>";
+echo  "[FORMULA] B = sha512(A)" . "<br>";
+echo  "<br>";
+echo  "B = " . $obj[bethash]  . "<br>";
+
+
+echo  "Genrating Roll From Hash <br>";
+echo  "[Formula] C = hexdec(substr(B, 0, 4) <br>";
+echo  "[Formula] D = C%(10000) <br>";
+echo  "[Formula] E = D/100 <br>";
+echo  "<br>";echo  "<br>";
+
+echo  "ROLL is :" . $obj[roll] ."<br>";
 
 
 ?>
