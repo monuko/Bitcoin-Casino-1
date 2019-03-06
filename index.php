266fd5e56f3bbf939c6206645e5eb4e695f01f48
Aman Singh <besoeasy@users.noreply.github.com>
2019-03-06 12:26:10 +0300

    Update index.php

--- index.php
+++ index.php
@@ -9,82 +9,6 @@
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 <script src="https://wchat.freshchat.com/js/widget.js"></script>
-
-<style>
-
-
-input[type="range"] { 
-    -webkit-appearance: none;
-    position: relative;
-    overflow: hidden;
-    height: 40px;
-    width: 400px;
-    cursor: pointer;
-    border-radius: 0;
-}
-
-::-webkit-slider-runnable-track {
-    background: #ddd;
-}
-
-/*
- * 1. Set to 0 width and remove border for a slider without a thumb
- */
-::-webkit-slider-thumb {
-    -webkit-appearance: none;
-    width: 20px; /* 1 */
-    height: 40px;
-    background: #fff;
-    box-shadow: -100vw 0 0 100vw dodgerblue;
-    border: 2px solid #999; /* 1 */
-}
-
-::-moz-range-track {
-    height: 40px;
-    background: #ddd;
-}
-
-::-moz-range-thumb {
-    background: #fff;
-    height: 40px;
-    width: 20px;
-    border: 3px solid #999;
-    border-radius: 0 !important;
-    box-shadow: -100vw 0 0 100vw dodgerblue;
-    box-sizing: border-box;
-}
-
-::-ms-fill-lower { 
-    background: dodgerblue;
-}
-
-::-ms-thumb { 
-    background: #fff;
-    border: 2px solid #999;
-    height: 40px;
-    width: 20px;
-    box-sizing: border-box;
-}
-
-::-ms-ticks-after { 
-    display: none; 
-}
-
-::-ms-ticks-before { 
-    display: none; 
-}
-
-::-ms-track { 
-    background: #ddd;
-    color: transparent;
-    height: 40px;
-    border: none;
-}
-
-::-ms-tooltip { 
-    display: none;
-}
-	</style>
 </head>
 <body>
 
@@ -111,11 +35,16 @@ input[type="range"] {
 </center>
 </div>
 
-<div class="col s12 l12">
+<div class="col s0 l2">
+</div>
+
+<div class="col s12 l8">
   <input type="range" min="2" max="40" value="4" id="myRange">
 </div>
 
 
+<div class="col s0 l2">
+</div>
 
 <div class="col s12 l4"><blockquote><center>
 <span id="maxbet">Maxbet 0.2 BTC</span></blockquote></center>
