53e403b8c747a00c2ef3f2fe73338c289482786e
Aman Singh <besoeasy@users.noreply.github.com>
2019-03-06 08:53:26 +0300

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,3 +1,4 @@
+<h3>
 <?php
 $trxn = htmlspecialchars($_GET["t"]);
 
@@ -17,14 +18,19 @@ echo  "[FORMULA] B = sha512(A)" . "<br>";
 echo  "<br>";
 echo  "B = " . $obj[bethash]  . "<br>";
 
+echo  "<br>";
+echo  "<br>";
+echo  "<br>";
 
 echo  "Genrating Roll From Hash <br>";
-echo  "[Formula] C = hexdec(substr(B, 0, 4) <br>";
-echo  "[Formula] D = C%(10000) <br>";
-echo  "[Formula] E = D/100 <br>";
-echo  "<br>";echo  "<br>";
+echo  "[FORMULA] C = hexdec(substr(B, 0, 4) <br>";
+echo  "[FORMULA] D = C%(10000) <br>";
+echo  "[FORMULA] E = D/100 <br>";
+echo  "<br>";
+echo  "<br>";
 
 echo  "ROLL is :" . $obj[roll] ."<br>";
 
 
 ?>
+</h3>
