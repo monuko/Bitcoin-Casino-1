24199a514714580cd8223e3dfda50cca11d1a26b
Aman Singh <besoeasy@users.noreply.github.com>
2019-03-31 22:58:58 +0530

    Update k.php

--- k.php
+++ k.php
@@ -1,4 +1,6 @@
 <?
+$i = 0;
+$p = 0;
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL,"https://slushpool.com/stats/json/btc/");
 curl_setopt($ch, CURLOPT_POST, 1);
@@ -14,13 +16,21 @@ $server_output = curl_exec($ch);
 curl_close($ch);
 $obj = json_decode($server_output, true);
 
+foreach($obj['btc']['blocks'] as $j){
+if($i<10){
+$p = $p + $j['mining_duration'];
+}
+$i = $i+1;
+}
+
+
 
 $conn = mysqli_connect('remotemysql.com', 'PY3gdINTnO', 'VaAWRokqsj', 'PY3gdINTnO');
 foreach($obj['btc']['blocks'] as $j){
 
 $j1 = $j['date_found'];
 $j2 = $j['mining_duration'];
-$j3 = "LUCK";
+$j3 = "0";
 
 $sql = "INSERT INTO part (date_found, mining_duration, luck) VALUES ('$j1', '$j2', '$j3') ";
 mysqli_query($conn, $sql);
