f0315cd0453e987ba43dc34dba0564e77d78c319
Aman Singh <besoeasy@users.noreply.github.com>
2019-03-31 21:43:44 +0530

    Update k.php

--- k.php
+++ k.php
@@ -6,25 +6,14 @@ curl_setopt($ch, CURLOPT_POSTFIELDS,$vars);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
 $headers = [
-    'SlushPool-Auth-Token: 6KYLH8Dz5Vo6qFNo',
-    'X-Apple-Tz: 0',
-    'X-Apple-Store-Front: 143444,12',
-    'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-    'Accept-Encoding: gzip, deflate',
-    'Accept-Language: en-US,en;q=0.5',
-    'Cache-Control: no-cache',
-    'Content-Type: application/x-www-form-urlencoded; charset=utf-8',
-    'Host: www.example.com',
-    'Referer: http://www.example.com/index.php', //Your referrer address
-    'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:28.0) Gecko/20100101 Firefox/28.0',
-    'X-MicrosoftAjax: Delta=true'
+    'SlushPool-Auth-Token: 6KYLH8Dz5Vo6qFNo'
 ];
 
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 
-$server_output = curl_exec ($ch);
+$server_output = curl_exec($ch);
 
-curl_close ($ch);
+curl_close($ch);
 
 print  $server_output ;
 ?>
