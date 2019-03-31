6017d6e02f846f2cd4a8597cf1f5600b191ff692
Aman Singh <besoeasy@users.noreply.github.com>
2019-03-31 23:01:15 +0530

    Create ok.php

---
+++ go/ok.php
@@ -0,0 +1,30 @@
+<?
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,"https://slushpool.com/stats/json/btc/");
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS,$vars); 
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+
+$headers = [
+    'SlushPool-Auth-Token: 6KYLH8Dz5Vo6qFNo'
+];
+
+
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+$server_output = curl_exec($ch);
+curl_close($ch);
+$obj = json_decode($server_output, true);
+
+if($obj['btc']['luck_b10'] < 1){
+echo "MOKA HE   ";
+}
+
+if($obj['btc']['luck_b50'] < 1){
+echo "Chode LE   ";
+}
+
+
+
+?>
