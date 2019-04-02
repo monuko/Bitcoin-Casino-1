0e2c298d547dc567a12e40706602d14f4a4a2493
Aman Singh <besoeasy@users.noreply.github.com>
2019-04-02 20:36:35 +0530

    Delete stake.php

--- go/stake.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-exec("curl 'https://api.stake.com/graphql' -H 'Accept-Encoding: gzip, deflate, br' -H 'Content-Type: application/json' -H 'Accept: application/json' -H 'Connection: keep-alive' -H 'DNT: 1' -H 'Origin: https://api.stake.com' -H 'x-access-token: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIyZjA3ODhlOS1mNThkLTRjNTgtYjk5NS01NWY3MDc0NGFlNWIiLCJzY29wZXMiOlsiYmV0Il0sImlhdCI6MTU1NDIxMjc2NSwiZXhwIjoxNTU5Mzk2NzY1fQ.sJOyRjQrCJLwX_Ef-1F733J56MWOnMWOytBW2QPTiKU' --data-binary '{"query":"mutation {\n  diceRoll (amount: 0.00000000, target: 49, condition: below, currency: btc) {\n    iid\n    payout\n    nonce\n  }\n}"}' --compressed");
-
-?>
