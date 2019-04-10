e9737b464556ae8b64c82c5f8e014e6e3c52be8d
Aman Singh <besoeasy@users.noreply.github.com>
2019-04-10 09:39:31 +0530

    Update paytm.php

--- go/paytm.php
+++ go/paytm.php
@@ -14,7 +14,7 @@
     // This is the staging value. Production value is available in your dashboard
     define("industryTypeId", "Retail");
     // This is the staging value. Production value is available in your dashboard
-    define("callbackUrl", "https://<Merchant_Response_URL>");
+    define("callbackUrl", "https://besoeasy.com");
     $paytmParams = array();
     $paytmParams["MID"] = merchantMid;
     $paytmParams["ORDER_ID"] = orderId;
