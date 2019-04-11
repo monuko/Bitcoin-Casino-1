95b4c2220b6983d8760579a1d2c656006baf0f0a
Aman Singh <besoeasy@users.noreply.github.com>
2019-04-12 04:20:51 +0530

    Update 1.php

--- go/1.php
+++ go/1.php
@@ -1,14 +1,24 @@
 <?php
+    $html = file_get_contents("http://www.amazon.in/gp/aw/d/B077Y3GCD4");
 
-    /* Enter the Amazon Product ISIN */
-    $amazonISIN = "B077Y3GCD4";
+function find_between(string $string, string $start, string $end, bool $greedy = false) {
+    $start = preg_quote($start);
+    $end   = preg_quote($end);
+ 
+    $format = '/(%s)(.*';
+    if (!$greedy) $format .= '?';
+    $format .= ')(%s)/';
+ 
+    $pattern = sprintf($format, $start, $end);
+    preg_match($pattern, $string, $matches);
+ 
+    return $matches[2];
+}
 
-    /* Grab the content of the HTML web page */
-    $html = file_get_contents("http://www.amazon.in/gp/aw/d/$amazonISIN");
 
-    /* Clean-up */
-    $html = str_replace("&amp;nbsp;", "", $html);
-
-echo $html;
+$string = $html;
+$start = "<span class="a-size-mini twisterSwatchPrice"> <span class="currencyINR">&nbsp;&nbsp;</span>";
+$end = "</span>";
+var_dump(find_between($string, $start, $end));
 
 ?>
