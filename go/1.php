d4903cc53230aa6c33d166b93a89d036febf0e91
Aman Singh <besoeasy@users.noreply.github.com>
2019-04-12 04:16:39 +0530

    Update 1.php

--- go/1.php
+++ go/1.php
@@ -9,16 +9,6 @@
     /* Clean-up */
     $html = str_replace("&amp;nbsp;", "", $html);
 
-    /* The magical regex for extracting the price */
-    $regex = '/\<b\>(Prezzo|Precio|Price|Prix Amazon|Preis):?\<\/b\>([^\<]+)/i';
-
-    /* Return the price */
-
-    if (preg_match($regex, $html, $price)) {
-        $price = number_format((float)($price[2]/100), 2, '.', '');
-        echo "The price for amazon.com/dp/$amazonISIN is $price";
-    } else {
-        echo "Sorry";
-    }
+echo $html;
 
 ?>
