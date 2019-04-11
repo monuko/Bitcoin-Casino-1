68fd5142f82dada444cbdbe3f3a8f6291acea21a
Aman Singh <besoeasy@users.noreply.github.com>
2019-04-12 04:24:20 +0530

    Update 1.php

--- go/1.php
+++ go/1.php
@@ -1,24 +1,20 @@
 <?php
     $html = file_get_contents("http://www.amazon.in/gp/aw/d/B077Y3GCD4");
 
-function find_between(string $string, string $start, string $end, bool $greedy = false) {
-    $start = preg_quote($start);
-    $end   = preg_quote($end);
- 
-    $format = '/(%s)(.*';
-    if (!$greedy) $format .= '?';
-    $format .= ')(%s)/';
- 
-    $pattern = sprintf($format, $start, $end);
-    preg_match($pattern, $string, $matches);
- 
-    return $matches[2];
+function getBetween($content,$start,$end){
+    $r = explode($start, $content);
+    if (isset($r[1])){
+        $r = explode($end, $r[1]);
+        return $r[0];
+    }
+    return '';
 }
 
 
-$string = $html;
-$start = "<span class="a-size-mini twisterSwatchPrice"> <span class="currencyINR">&nbsp;&nbsp;</span>";
-$end = "</span>";
-var_dump(find_between($string, $start, $end));
+$content = $html;
+$start = "data-asin-price=";
+$end = " data-asin-shipping ";
+$output = getBetween($content,$start,$end);
+echo $output;
 
 ?>
