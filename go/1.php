bcc240985eaeb6d1192264c4e3b266e0b6a03100
Aman Singh <besoeasy@users.noreply.github.com>
2019-04-12 15:10:04 +0530

    Update 1.php

--- go/1.php
+++ go/1.php
@@ -1,9 +1,13 @@
 <?php
+function clean($string) {
+   return preg_replace('/[^A-Za-z0-9\-]/', '', $string); // Removes special chars.
+}
+
 
     $url="https://www.amazon.in/dp/B07DJHV6VZ/";
     $url =  file_get_contents($url);
 
 
-echo $url;
+echo clean($url);
 
 ?>
