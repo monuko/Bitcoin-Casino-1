3c606656b878201af5e27d4cab7d6c7b8b742b5a
Aman Singh <besoeasy@users.noreply.github.com>
2019-04-12 15:05:21 +0530

    Update 1.php

--- go/1.php
+++ go/1.php
@@ -1,22 +1,7 @@
 <?php
- 
-function getHTMLcode($url) {
-	
-	$curl = curl_init($url);
-	curl_setopt($curl, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 10.10; labnol;) ctrlq.org");
-	curl_setopt($curl, CURLOPT_FAILONERROR, true);
-	curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
-	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-	$html = curl_exec($curl);
-	curl_close($curl);
-	
-	return $html;
-		
-}
-
-
 
     $url="https://www.amazon.in/dp/B07DJHV6VZ/";
+    $url =  file_get_contents($url);
     $html= getHTMLcode($url);
 
 
