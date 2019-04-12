c0d543230e8ac2cf9ef677df875da7a1c082a44c
Aman Singh <besoeasy@users.noreply.github.com>
2019-04-12 15:23:38 +0530

    Update 1.php

--- go/1.php
+++ go/1.php
@@ -1,7 +1,5 @@
 <?php
-function clean($string) {
-   return preg_replace('/[^A-Za-z0-9\-]/', '', $string); // Removes special chars.
-}
+
 
 function extract_unit($string, $start, $end){
 $pos = stripos($string, $start);
@@ -17,8 +15,8 @@ return $unit;
 
     $url="https://www.amazon.in/dp/B07DJHV6VZ/";
     $url =  file_get_contents($url);
-    $data= clean(extract_unit($url, 'asin-price=', 'data-asin-shipping'));
-
+    $data= extract_unit($url, 'asin-price=', 'data-asin-shipping');
+    $data = str_replace('"',"",$data)
 
 
 
