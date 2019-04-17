7b881dc8642561e711a521753619b11878540461
Aman Singh <besoeasy@users.noreply.github.com>
2019-04-18 02:02:54 +0530

    Update okay.php

--- go/okay.php
+++ go/okay.php
@@ -8,11 +8,8 @@ $id = $obj['result']['orders'][0]['id'];
 $newlimit = $limit*1.1;
 
 
-//$url2 = "https://api.nicehash.com/api?method=orders.set.limit&my&id=193027&key=9295f08b-d659-a348-7b1d-365539733937&location=0&algo=1&&order=". $id ."&limit=" . $newlimit ;
-//$result =  file_get_contents($url2);
-
-echo $id . "<br>";
-echo $newlimit . "<br>";
-//echo $result;
+$url2 = "https://api.nicehash.com/api?method=orders.set.limit&my&id=193027&key=9295f08b-d659-a348-7b1d-365539733937&location=0&algo=1&&order=$id&limit=$newlimit" ;
+$result =  file_get_contents($url2);
+echo $result;
 ?>
 
