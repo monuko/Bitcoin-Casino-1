f029a26e7c7244302d7682051d4969e2854fc042
Aman Singh <besoeasy@users.noreply.github.com>
2019-04-20 23:23:16 +0100

    Update okay.php

--- go/okay.php
+++ go/okay.php
@@ -1,36 +1,118 @@
 <meta http-equiv="refresh" content="<? echo rand(1,22); ?>">
 
+
+
 <?
+$ab = 0;
+$p = 0;
+$s = 10000000;
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,"https://slushpool.com/stats/json/btc/");
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS,$vars); 
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+
+$headers = [
+    'SlushPool-Auth-Token: 6KYLH8Dz5Vo6qFNo'
+];
+
+
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+$server_output = curl_exec($ch);
+curl_close($ch);
+$obj = json_decode($server_output, true);
+
+
+foreach($obj['btc']['blocks'] as $j){
+if($p<$j['mining_duration']){
+$p =  $j['mining_duration'];
+}
+
+if($s>$j['mining_duration']){
+$s =  $j['mining_duration'];
+}
+
+$ab = $j['mining_duration'] + $ab;
+}
+
+
+$totalmine = $ab/15;
+$x = $obj['btc']['round_duration'];
+
+
+if($obj['btc']['luck_b10'] < 1){
+echo "Moka He  ";
+}
+
+
+if($obj['btc']['luck_b50'] < 1){
+echo "Chod Le.. ";
+}
+
+if($totalmine < $x ){
+echo "Zor Se.. ";
+}
+
+
+echo "<br> ______________________________________" ;
+echo "<br> Current Luck " . $obj['btc']['round_probability'];
+echo "<br> 10 Luck " . $obj['btc']['luck_b10'];
+echo "<br> 50 Luck " . $obj['btc']['luck_b50'];
+
+echo "<br> Avg Block $totalmine Sec";
+echo "<br> No Block From Last  $x Sec";
+echo "<br> Longest Block Yet $p Sec";
+echo "<br> Short Block Yet $s Sec";
+
+
+
 //pool check
 $block = json_decode(file_get_contents("https://chain.api.btc.com/v3/block/latest"), true); 
 $pool = $block['data']['extras']['pool_link'];
 
 //nicehash get
 $url = "https://api.nicehash.com/api?method=orders.get&my&id=193027&key=9295f08b-d659-a348-7b1d-365539733937&location=0&algo=1";
-$obj = json_decode(file_get_contents($url), true);
+$obj9 = json_decode(file_get_contents($url), true);
 
 //logic
-$limit = $obj['result']['orders'][0]['limit_speed'];
-$id = $obj['result']['orders'][0]['id'];
-$newlimit = $limit*1.1;
+$limit = $obj9['result']['orders'][0]['limit_speed'];
+$id = $obj9['result']['orders'][0]['id'];
+
+
+//$newlimit = $limit*1.1;
+$newlimit = 1 + $obj['btc']['round_probability'] + $obj['btc']['round_probability'];
+
+
 $result = $limit;
+$rlimit = 0.2;
+
+if($obj['btc']['luck_b10'] < 1){
+$rlimit = 0.4;
+}
+
+if($obj['btc']['luck_b50'] < 1){
+$rlimit = $rlimit + 0.2;
+}
 
 
 
 if (strpos($pool, 'slushpool') !== false) {
 echo 'RESET LIMIT';
 
-$url2 = "https://api.nicehash.com/api?method=orders.set.limit&my&id=193027&key=9295f08b-d659-a348-7b1d-365539733937&location=0&algo=1&&order=$id&limit=0.4" ;
+$url2 = "https://api.nicehash.com/api?method=orders.set.limit&my&id=193027&key=9295f08b-d659-a348-7b1d-365539733937&location=0&algo=1&&order=$id&limit=$rlimit" ;
 $result =  file_get_contents($url2);
 }else{
 
-if(rand(1,100)<2){    
+if(rand(1,10)<2){    
 $url2 = "https://api.nicehash.com/api?method=orders.set.limit&my&id=193027&key=9295f08b-d659-a348-7b1d-365539733937&location=0&algo=1&&order=$id&limit=$newlimit" ;
 $result =  file_get_contents($url2);
 }    
 
 }
 
-echo $result;
+echo "<br> limit to Set : $newlimit";
+echo "<br> if Block Found, then limit : $rlimit";
+echo "<br> Current Bid $result";
 ?>
 
