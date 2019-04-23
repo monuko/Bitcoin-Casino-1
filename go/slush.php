7ba0ffe7ce455b477b1a4cd24b499ba12838b662
Aman Singh <besoeasy@users.noreply.github.com>
2019-04-23 17:02:04 +0530

    Update slush.php

--- go/slush.php
+++ go/slush.php
@@ -99,7 +99,7 @@ $newlimit = ($rlimit + $obj['btc']['round_probability'] + $obj['btc']['round_pro
 if (strpos($pool, 'slushpool') !== false) {
 echo 'RESET LIMIT';
 
-$url2 = "https://api.nicehash.com/api?method=orders.set.limit&my&id=193027&key=9295f08b-d659-a348-7b1d-365539733937&location=0&algo=1&&order=$id&limit=$rlimit" ;
+$url2 = "https://api.nicehash.com/api?method=orders.set.limit&my&id=193027&key=9295f08b-d659-a348-7b1d-365539733937&location=0&algo=1&&order=$id&limit=0.1" ;
 $result =  file_get_contents($url2);
 }else{
 
