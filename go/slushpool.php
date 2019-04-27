c6bbcc0605c647018224ebdf2f61dfd1608a9aef
Aman Singh <besoeasy@users.noreply.github.com>
2019-04-27 14:41:12 +0530

    Update slushpool.php

--- go/slushpool.php
+++ go/slushpool.php
@@ -86,8 +86,8 @@ if($newlimit<0.05){
 $newlimit = 0.1;
 }
  
-//$url2 = "https://api.nicehash.com/api?method=orders.set.limit&my&id=193027&key=9295f08b-d659-a348-7b1d-365539733937&location=0&algo=1&&order=$id&limit=$newlimit" ;
-//$result =  file_get_contents($url2);
+$url2 = "https://api.nicehash.com/api?method=orders.set.limit&my&id=193027&key=9295f08b-d659-a348-7b1d-365539733937&location=0&algo=1&&order=$id&limit=$newlimit" ;
+$result =  file_get_contents($url2);
 
 
 ///////////////////////////
