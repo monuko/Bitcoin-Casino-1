29fe3456e07663295d94d454df93d290c7410b1a
Aman Singh <besoeasy@users.noreply.github.com>
2019-04-29 05:00:24 +0530

    Update ok.php

--- go/ok.php
+++ go/ok.php
@@ -2,20 +2,22 @@
 
 <?
 $i = 0;
-$ii =0;
 $url = "https://api.smartbit.com.au/v1/blockchain/blocks?limit=100";
 $obj9 = json_decode(file_get_contents($url), true);
+
 
 foreach($obj9['blocks'] as $j){
 if(!strcmp($j['pool']['name'], "SlushPool")){
 $i = $i+1;
+$a[$i] = $j['confirmations'] ;
 
 echo $j['confirmations'] . " -> " . $j['pool']['name'] . "<br>";
 
-$ii = $ii + (100 - $j['confirmations']);
 }
 }
 
+$p = ($a[1] + $a[2] + $a[3])/3;
 
-echo "POWER IS $ii    Total Blocks $i -> %age of  " . (100/$i);
+
+echo "$p      Total Blocks $i -> %age of  " . (100/$i);
 ?>
