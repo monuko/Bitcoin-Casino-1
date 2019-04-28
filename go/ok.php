2ae153a580a8a736a97c1af0976a46b573021364
Aman Singh <besoeasy@users.noreply.github.com>
2019-04-29 04:45:16 +0530

    Update ok.php

--- go/ok.php
+++ go/ok.php
@@ -1,14 +1,19 @@
 <meta http-equiv="refresh" content="300">
 
 <?
+$i = 0;
 $url = "https://api.smartbit.com.au/v1/blockchain/blocks?limit=300";
 $obj9 = json_decode(file_get_contents($url), true);
 
 foreach($obj9['blocks'] as $j){
 if(!strcmp($j['pool']['name'], "SlushPool")){
+$i = $i+1;
 
 echo $j['confirmations'] . " -> " . $j['pool']['name'] . "<br>";
 
 }
 }
+
+
+echo "Total Blocks $i -> %age of  " . (300/$i);
 ?>
