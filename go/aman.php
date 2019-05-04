2fbe3b389d82a1056b6494d370e6698422ba7f9f
Aman Singh <besoeasy@users.noreply.github.com>
2019-05-05 04:41:38 +0530

    Update aman.php

--- go/aman.php
+++ go/aman.php
@@ -16,6 +16,17 @@ $server_output = curl_exec($ch);
 curl_close($ch);
 $obj = json_decode($server_output, true);
 
-echo $obj['btc']['estimated_reward'];
+$reward =  $obj['btc']['estimated_reward'];
 
+
+//nicehash get
+$url = "https://api.nicehash.com/api?method=orders.get&my&id=193027&key=9295f08b-d659-a348-7b1d-365539733937&location=0&algo=1";
+$obj9 = json_decode(file_get_contents($url), true);
+
+$paid = $obj9['result']['orders'][0]['btc_paid'];
+
+
+if($reward> $paid){
+echo "In Profit";
+}
 ?>
