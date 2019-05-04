5413760de05f7d9787ecad91939c5d867e167e36
Aman Singh <besoeasy@users.noreply.github.com>
2019-05-05 05:11:32 +0530

    Update aman.php

--- go/aman.php
+++ go/aman.php
@@ -29,33 +29,27 @@ $paid = $obj9['result']['orders'][0]['btc_paid'];
 $limit = $obj9['result']['orders'][0]['limit_speed'];
 $id = $obj9['result']['orders'][0]['id'];
 
-
-
-
 $obj5 = json_decode(file_get_contents("https://api.smartbit.com.au/v1/blockchain/blocks?limit=5"), true);
 
 
 foreach($obj5['blocks'] as $j){
 if(!strcmp($j['pool']['name'], "SlushPool")){
 $boonk = 1;
-}
-}
+}}
 
 
 if($boonk>0){
-    
 $url2 = "https://api.nicehash.com/api?method=orders.set.limit&my&id=193027&key=9295f08b-d659-a348-7b1d-365539733937&location=0&algo=1&&order=$id&limit=0.05" ;
 $result =  file_get_contents($url2);
-
 echo $result;
-    
 }
 
 
 echo "Limit -> $limit PH | Paid -> $paid | Reward -> $reward <br>";
-$rewardx = 1.1*$reward'
+$rewardx = 1.1*$reward;
 
 if( $rewardx  > $paid){
+
 echo "In Profit <br> ";
 
 }else{
@@ -65,10 +59,9 @@ if($boonk<1){
 $newlimit = $limit*1.1;
 $url2 = "https://api.nicehash.com/api?method=orders.set.limit&my&id=193027&key=9295f08b-d659-a348-7b1d-365539733937&location=0&algo=1&&order=$id&limit=$newlimit" ;
 $result =  file_get_contents($url2);
-
 echo $result;
-}
-}
+
+}}
 
 
 
