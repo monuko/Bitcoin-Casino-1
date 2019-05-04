4d214e40b21fc41244e6c99fe18c760eb109102c
Aman Singh <besoeasy@users.noreply.github.com>
2019-05-05 04:49:57 +0530

    Update aman.php

--- go/aman.php
+++ go/aman.php
@@ -32,11 +32,15 @@ $limit = $obj9['result']['orders'][0]['limit_speed'];
 $id = $obj9['result']['orders'][0]['id'];
 
 
+
+echo "Limit -> $limit PH | Paid -> $paid | Reward -> $reward <br>";
+
+
 if( (1.05*$reward) > $paid){
-echo "In Profit ($limit PH) <br> ";
-echo " Paid -> $paid | Reward -> $reward ";
+echo "In Profit <br> ";
 
 }else{
+
 $newlimit = $limit*1.1;
 $url2 = "https://api.nicehash.com/api?method=orders.set.limit&my&id=193027&key=9295f08b-d659-a348-7b1d-365539733937&location=0&algo=1&&order=$id&limit=$newlimit" ;
 $result =  file_get_contents($url2);
