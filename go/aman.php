2c01ca102163844d95e6d6b3cd7d8b5823ed2bc7
Aman Singh <besoeasy@users.noreply.github.com>
2019-05-05 04:39:03 +0530

    Create aman.php

---
+++ go/aman.php
@@ -0,0 +1,21 @@
+<?
+
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,"https://slushpool.com/accounts/profile/json/btc/");
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS,$vars); 
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$headers = [
+    'SlushPool-Auth-Token: 6KYLH8Dz5Vo6qFNo'
+];
+
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+$server_output = curl_exec($ch);
+curl_close($ch);
+$obj = json_decode($server_output, true);
+
+echo $obj['btc']['estimated_reward'];
+
+?>
