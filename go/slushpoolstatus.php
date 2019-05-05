077968a227a9292c028161a4d530d3de18f79c9b
Aman Singh <besoeasy@users.noreply.github.com>
2019-05-06 03:06:21 +0530

    Create slushpoolstatus.php

---
+++ go/slushpoolstatus.php
@@ -0,0 +1,31 @@
+<meta http-equiv="refresh" content="170">
+
+<?
+$i = 0;
+$url = "https://api.smartbit.com.au/v1/blockchain/blocks?limit=600";
+$obj9 = json_decode(file_get_contents($url), true);
+
+
+$obj2 = json_decode(file_get_contents("https://api.nicehash.com/api?method=orders.get&my&id=193027&key=9295f08b-d659-a348-7b1d-365539733937&location=0&algo=1"), true);
+$limit = $obj2['result']['orders'][0]['limit_speed'];
+$id = $obj2['result']['orders'][0]['id'];
+
+
+
+foreach($obj9['blocks'] as $j){
+if(!strcmp($j['pool']['name'], "SlushPool")){
+$i = $i+1;
+$a[$i] = $j['confirmations'] ;
+
+echo $j['confirmations'] . " -> " . $j['pool']['name'] . "<br>";
+
+}
+}
+
+
+
+
+
+
+echo "   Total Blocks $i";
+?>
