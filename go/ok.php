de0e60d9fd97ae8d331f92174c873677f97da8d6
Aman Singh <besoeasy@users.noreply.github.com>
2019-05-05 16:36:38 +0530

    Update ok.php

--- go/ok.php
+++ go/ok.php
@@ -17,27 +17,8 @@ echo $j['confirmations'] . " -> " . $j['pool']['name'] . "<br>";
 }
 
 
-$p = $a[1]/10;
-
-if($p>1.9){
-$p = $a[1]/7;
-}  
 
 
-//nicehash get
-$url = "https://api.nicehash.com/api?method=orders.get&my&id=193027&key=9295f08b-d659-a348-7b1d-365539733937&location=0&algo=1";
-$obj9 = json_decode(file_get_contents($url), true);
-$id = $obj9['result']['orders'][0]['id'];
-
-//nicehash set order
-$url2 = "https://api.nicehash.com/api?method=orders.set.limit&my&id=193027&key=9295f08b-d659-a348-7b1d-365539733937&location=0&algo=1&&order=$id&limit=$p" ;
-$result =  file_get_contents($url2);
-
-
-
-
-
-
-echo "$p PH/s  <br>   Total Blocks $i";
+echo "Total Blocks $i";
 ?>
 
