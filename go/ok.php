261ceeb7048d358742715fce0a95ec3bd8a66db6
Aman Singh <besoeasy@users.noreply.github.com>
2019-05-05 23:56:58 +0530

    Update ok.php

--- go/ok.php
+++ go/ok.php
@@ -6,6 +6,12 @@ $url = "https://api.smartbit.com.au/v1/blockchain/blocks?limit=300";
 $obj9 = json_decode(file_get_contents($url), true);
 
 
+$obj2 = json_decode(file_get_contents("https://api.nicehash.com/api?method=orders.get&my&id=193027&key=9295f08b-d659-a348-7b1d-365539733937&location=0&algo=1"), true);
+$limit = $obj2['result']['orders'][0]['limit_speed'];
+$id = $obj2['result']['orders'][0]['id'];
+
+
+
 foreach($obj9['blocks'] as $j){
 if(!strcmp($j['pool']['name'], "SlushPool")){
 $i = $i+1;
@@ -19,25 +25,20 @@ echo $j['confirmations'] . " -> " . $j['pool']['name'] . "<br>";
 //logic
 $p = $a[1]/10;
 
-if($a[1]>17){
-$p = $a[1]/8;
-}  
+if($a[1]>12){
 
-if($a[1]>23){
-$p = $a[1]/7;
-}  
+$newlimit = $limit*($a[1]/10);
+$url2 = "https://api.nicehash.com/api?method=orders.set.limit&my&id=193027&key=9295f08b-d659-a348-7b1d-365539733937&location=0&algo=1&&order=$id&limit=$newlimit" ;
+$result =  file_get_contents($url2);
+echo $result;
 
-
-
-//nicehash get
-$url = "https://api.nicehash.com/api?method=orders.get&my&id=193027&key=9295f08b-d659-a348-7b1d-365539733937&location=0&algo=1";
-$obj9 = json_decode(file_get_contents($url), true);
-$id = $obj9['result']['orders'][0]['id'];
+}else{
 
 //nicehash set order
 $url2 = "https://api.nicehash.com/api?method=orders.set.limit&my&id=193027&key=9295f08b-d659-a348-7b1d-365539733937&location=0&algo=1&&order=$id&limit=$p" ;
 $result =  file_get_contents($url2);
 
+}  
 
 
 
