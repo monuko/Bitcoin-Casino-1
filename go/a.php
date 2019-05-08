91c83fae146626aefc9aec1c6ec7ff0b60a5bf30
Aman Singh <besoeasy@users.noreply.github.com>
2019-05-08 21:49:57 +0530

    Update a.php

--- go/a.php
+++ go/a.php
@@ -2,15 +2,18 @@
 $inrwin2 = $_GET["inrwin"];
 $upi = $_GET["upi"];
 $txnid = $_GET["txnid"];
-
-
 $conn = mysqli_connect("remotemysql.com", "PY3gdINTnO", "VaAWRokqsj", "PY3gdINTnO");
-$inramount2 = $conn->query("SELECT bal FROM PY3gdINTnO.bal WHERE upi='$upi' AND txnid=$txnid")->fetch_object()->bal; 
+$result = $conn->query("SELECT bal FROM PY3gdINTnO.bal WHERE upi='$upi' AND txnid=$txnid"); 
+
+$inramount2 = $result ->fetch_object()->bal;
+$ticketid = $result ->fetch_object()->ticketid; 
 
 
 echo $inramount2;
 
-
+if($ticketid<1){
+echo "   CAN BET NOW";
+}
 
 
 mysqli_close($conn);
