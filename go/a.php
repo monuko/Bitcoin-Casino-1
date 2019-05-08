53b744f9585445f86824c0b7896f761674e476b9
Aman Singh <besoeasy@users.noreply.github.com>
2019-05-08 21:43:48 +0530

    Update a.php

--- go/a.php
+++ go/a.php
@@ -5,7 +5,7 @@ $txnid = $_GET["txnid"];
 
 
 $conn = mysqli_connect("remotemysql.com", "PY3gdINTnO", "VaAWRokqsj", "PY3gdINTnO");
-$sql = "SELECT bal FROM PY3gdINTnO.bal WHERE upi=$upi AND txnid=$txnid";
+$sql = "SELECT bal FROM PY3gdINTnO.bal WHERE upi='$upi' AND txnid=$txnid";
 echo $sql;
 
 $inramount2 = $conn->query($sql)->fetch_object()->bal; 
