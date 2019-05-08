bc44cfaf66b31b0f2599d9440c0c86212f381956
Aman Singh <besoeasy@users.noreply.github.com>
2019-05-08 21:21:57 +0530

    Update a.php

--- go/a.php
+++ go/a.php
@@ -1,10 +1,5 @@
 <?php
 $conn = mysqli_connect("remotemysql.com", "PY3gdINTnO", "VaAWRokqsj", "PY3gdINTnO");
-// Check connection
-if (!$conn) {
-    die("Connection failed !!");
-}
-
 
 $name = $conn->query("SELECT bal FROM PY3gdINTnO.bal LIMIT 1")->fetch_object()->bal; 
 
