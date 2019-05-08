32fc466671c12721856baa3a3134ccaed4168d06
Aman Singh <besoeasy@users.noreply.github.com>
2019-05-08 21:03:22 +0530

    Update a.php

--- go/a.php
+++ go/a.php
@@ -1,15 +1,15 @@
-<?
-$conn = mysqli_connect('remotemysql.com', 'PY3gdINTnO', 'VaAWRokqsj', 'PY3gdINTnO');
 
+<?php
+$servername = "remotemysql.com";
+$username = "PY3gdINTnO";
+$password = "VaAWRokqsj";
 
+// Create connection
+$conn = new mysqli($servername, $username, $password);
 
-//$sql = "UPDATE PY3gdINTnO.bal SET bal=bal-$inramount2 WHERE upi=$upi AND txnid=$txnid LIMIT 1";
-$sql = "SELECT bal FROM PY3gdINTnO.bal LIMIT 1";
-
-$result = mysqli_query($conn, $sql);
-
-
-
-echo $result;
-
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
 ?>
