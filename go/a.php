f600042df1b568921bae0542f47554831dc50809
Aman Singh <besoeasy@users.noreply.github.com>
2019-05-08 21:29:33 +0530

    Update a.php

--- go/a.php
+++ go/a.php
@@ -1,16 +1,15 @@
 <?php
-$inramount2 = $_GET["inr"];
 $inrwin2 = $_GET["inrwin"];
 $upi = $_GET["upi"];
 $txnid = $_GET["txnid"];
 
 
 $conn = mysqli_connect("remotemysql.com", "PY3gdINTnO", "VaAWRokqsj", "PY3gdINTnO");
-$inramount2 = $conn->query("SELECT bal FROM PY3gdINTnO.bal WHERE  upi=$upi AND txnid=$txnid LIMIT 1")->fetch_object()->bal; 
+$inramount2 = $conn->query("SELECT bal FROM PY3gdINTnO.bal WHERE  upi=". $upi." AND txnid=".$txnid." LIMIT 1")->fetch_object()->bal; 
 echo $inramount2;
 
 if($inramount2>0){
-$conn->query("DELETE FROM PY3gdINTnO.bal WHERE upi=$upi AND txnid=$txnid"); 
+$conn->query("DELETE FROM PY3gdINTnO.bal WHERE upi=".$upi." AND txnid=".$txnid); 
 echo "  DELETED "
 }
 
