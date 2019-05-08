5915fc17510438c0f7bbb434ed25db4a56f4e8c6
Aman Singh <besoeasy@users.noreply.github.com>
2019-05-08 20:59:03 +0530

    Update a.php

--- go/a.php
+++ go/a.php
@@ -4,16 +4,12 @@ $conn = mysqli_connect('remotemysql.com', 'PY3gdINTnO', 'VaAWRokqsj', 'PY3gdINTn
 
 
 //$sql = "UPDATE PY3gdINTnO.bal SET bal=bal-$inramount2 WHERE upi=$upi AND txnid=$txnid LIMIT 1";
-$sql2 = "SELECT bal FROM PY3gdINTnO.bal LIMIT 1";
+$sql = "SELECT bal FROM PY3gdINTnO.bal LIMIT 1";
 
 $result = mysqli_query($conn, $sql);
-$result2 = mysqli_query($conn, $sql2);
 
-$row=mysql_fetch_row($result2);
 
-if($row[0] >= 0) {
-echo " HELLO SIR";
-}
 
+echo $result;
 
 ?>
