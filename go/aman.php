d1a90e8769405e5f55f717be081f5894295798b4
Aman Singh <besoeasy@users.noreply.github.com>
2019-05-13 04:59:45 +0530

    Update aman.php

--- go/aman.php
+++ go/aman.php
@@ -54,11 +54,6 @@ echo $result;
 }
 
 
-if( $reward > $paidx){
-echo "In Profit <br> ";
-}
-
-
 if ( $boonk<1 && $reward < $paidx ){
 $newlimit = $limit*1.21;
 $url2 = "https://api.nicehash.com/api?method=orders.set.limit&my&id=193027&key=9295f08b-d659-a348-7b1d-365539733937&location=0&algo=1&&order=$id&limit=$newlimit" ;
