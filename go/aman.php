fdbaf96a8e9e449e680a2537928f175daf9c9bc4
Aman Singh <besoeasy@users.noreply.github.com>
2019-05-13 04:32:14 +0530

    Update aman.php

--- go/aman.php
+++ go/aman.php
@@ -3,7 +3,6 @@
 
 
 <?
-$err = trim(file_get_contents('err.txt'));
 
 
 $boonk = 0;
@@ -18,6 +17,7 @@ $headers = [
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 $server_output = curl_exec($ch);
 curl_close($ch);
+
 $obj = json_decode($server_output, true);
 $obj9 = json_decode(file_get_contents("https://api.nicehash.com/api?method=orders.get&my&id=193027&key=9295f08b-d659-a348-7b1d-365539733937&location=0&algo=1"), true);
 $obj5 = json_decode(file_get_contents("https://api.smartbit.com.au/v1/blockchain/blocks?limit=4"), true);
@@ -31,12 +31,16 @@ $paidx = 1.21*$paid;
 $limit = $obj9['result']['orders'][0]['limit_speed'];
 $id = $obj9['result']['orders'][0]['id'];
 $reward =  $obj['btc']['estimated_reward'] + $obj['btc']['unconfirmed_reward'] ;
-$reward = $reward - $err;
+
+
 //if block found
 foreach($obj5['blocks'] as $j){
 if(!strcmp($j['pool']['name'], "SlushPool")){
 $boonk = 1;
 }}
+
+
+
 //print details
 echo "Limit -> $limit PH | Paid -> $paid | Reward -> $reward <br>";
 
