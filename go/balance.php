9bb5f20277ca9b5d188236f8d3152fa206a6c63e
Aman Singh <besoeasy@users.noreply.github.com>
2019-05-13 22:58:34 +0530

    Update balance.php

--- go/balance.php
+++ go/balance.php
@@ -1,3 +1,6 @@
+<meta http-equiv="refresh" content="55">
+
+
 <?
 $result =  file_get_contents("https://api.nicehash.com/api?method=balance&id=193027&key=9295f08b-d659-a348-7b1d-365539733937");
 $obj = json_decode($result, true);
