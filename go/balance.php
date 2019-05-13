9dfe0c3a41bf52b5e193f3238ac6e8e53f0cb48e
Aman Singh <besoeasy@users.noreply.github.com>
2019-05-13 22:58:16 +0530

    Update balance.php

--- go/balance.php
+++ go/balance.php
@@ -1,7 +1,8 @@
 <?
 $result =  file_get_contents("https://api.nicehash.com/api?method=balance&id=193027&key=9295f08b-d659-a348-7b1d-365539733937");
 $obj = json_decode($result, true);
+$bal = $obj['result']['balance_confirmed'] + $obj['result']['balance_pending']  ;
 
-echo $obj['result']['balance_confirmed'];
 
+echo "<h1>" . $bal . "</h1>;
 ?>
