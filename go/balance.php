0ddbcf9c961cc6874ec58ca9be756d07d2e250fe
Aman Singh <besoeasy@users.noreply.github.com>
2019-05-13 23:23:22 +0530

    Update balance.php

--- go/balance.php
+++ go/balance.php
@@ -1,5 +1,22 @@
 <meta http-equiv="refresh" content="55">
 
+<style>
+
+div {
+	width: 100px;
+	height: 100px;	
+	position: absolute;
+	top:0;
+	bottom: 0;
+	left: 0;
+	right: 0;
+	margin: auto;
+}
+
+</style>
+<div>
+
+
 
 <?
 $result =  file_get_contents("https://api.nicehash.com/api?method=balance&id=193027&key=9295f08b-d659-a348-7b1d-365539733937");
@@ -15,5 +32,8 @@ $c= $inr * $bal;
 $c= round($c);
 
 
-echo "<center><font size=90> " . $bal . " BTC  ->  " . $c  . " INR    </font></center>";
+echo "<font size=90> " . $bal . " BTC  ->  " . $c  . " INR    </font>";
 ?>
+
+
+</div>
