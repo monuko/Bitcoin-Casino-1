ab6a4921578fefa7b038a8c054e0ce78e9b92bf8
Aman Singh <besoeasy@users.noreply.github.com>
2019-05-15 08:53:29 +0530

    Update slushpoolstatus.php

--- go/slushpoolstatus.php
+++ go/slushpoolstatus.php
@@ -22,5 +22,38 @@ echo  $a[$i] . " -> "  . ($a[$i] -$a[$i-1]) . "<br>";
 }
 }
 
-echo " Total Blocks $i";
+echo " Total Blocks $i <br>";
+
+
+
+
+
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,"https://slushpool.com/accounts/profile/json/btc/");
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS,$vars); 
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$headers = [
+    'SlushPool-Auth-Token: 6KYLH8Dz5Vo6qFNo'
+];
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+$server_output = curl_exec($ch);
+curl_close($ch);
+
+$obj = json_decode($server_output, true);
+$obj9 = json_decode(file_get_contents("https://api.nicehash.com/api?method=orders.get&my&id=193027&key=9295f08b-d659-a348-7b1d-365539733937&location=0&algo=1"), true);
+
+
+//vars
+$paid = $obj9['result']['orders'][0]['btc_paid'];
+$limit = $obj9['result']['orders'][0]['limit_speed'];
+$id = $obj9['result']['orders'][0]['id'];
+$reward =  $obj['btc']['estimated_reward'] + $obj['btc']['unconfirmed_reward'] ;
+
+
+//print details
+echo "Limit -> $limit PH | Paid -> $paid | Reward -> $reward <br>";
+echo $reward/$paid  ."     - Profit Ratio<br>";
+echo ($reward-$paid)  ."     - Profit BTC<br>";
 ?>
