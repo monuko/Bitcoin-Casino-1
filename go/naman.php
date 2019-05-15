566fd93ab49d801a72313567dc4de864131bd4a2
Aman Singh <besoeasy@users.noreply.github.com>
2019-05-15 09:28:18 +0530

    Create naman.php

---
+++ go/naman.php
@@ -0,0 +1,66 @@
+<meta http-equiv="refresh" content="255">
+
+
+
+<?
+
+
+$boonk = 0;
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,"https://slushpool.com/accounts/profile/json/btc/");
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS,$vars); 
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$headers = [
+    'SlushPool-Auth-Token: 6KYLH8Dz5Vo6qFNo'
+];
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+$server_output = curl_exec($ch);
+curl_close($ch);
+
+$obj = json_decode($server_output, true);
+$obj9 = json_decode(file_get_contents("https://api.nicehash.com/api?method=orders.get&my&id=193027&key=9295f08b-d659-a348-7b1d-365539733937&location=0&algo=1"), true);
+$obj5 = json_decode(file_get_contents("https://api.smartbit.com.au/v1/blockchain/blocks?limit=4"), true);
+
+
+//vars
+$paid = $obj9['result']['orders'][0]['btc_paid'];
+
+
+
+$limit = $obj9['result']['orders'][0]['limit_speed'];
+$id = $obj9['result']['orders'][0]['id'];
+$reward =  $obj['btc']['estimated_reward'] ;
+
+
+//if block found
+foreach($obj5['blocks'] as $j){
+if(!strcmp($j['pool']['name'], "SlushPool")){
+$boonk = 1;
+}}
+
+
+
+//print details
+echo "Limit -> $limit PH | Paid -> $paid | Reward -> $reward <br>";
+echo $reward/$paid  ."     - Profit Ratio<br>";
+echo ($reward-$paid)  ."     - Profit BTC<br>";
+
+//logics
+if($boonk>0){
+$url2 = "https://api.nicehash.com/api?method=orders.set.limit&my&id=193027&key=9295f08b-d659-a348-7b1d-365539733937&location=0&algo=1&&order=$id&limit=0.05" ;
+$result =  file_get_contents($url2);
+echo $result;
+}
+
+
+if ( $boonk<1 && ($reward/$paid) < 1.51 ){
+$newlimit = $limit*1.5;
+$url2 = "https://api.nicehash.com/api?method=orders.set.limit&my&id=193027&key=9295f08b-d659-a348-7b1d-365539733937&location=0&algo=1&&order=$id&limit=$newlimit" ;
+$result =  file_get_contents($url2);
+echo $result;
+}
+
+
+
+?>
