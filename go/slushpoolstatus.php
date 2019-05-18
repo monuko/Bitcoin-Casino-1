4f627aaa1eae617660224ff6759aa6fa6c07f603
Aman Singh <besoeasy@users.noreply.github.com>
2019-05-18 15:06:47 +0530

    Update slushpoolstatus.php

--- go/slushpoolstatus.php
+++ go/slushpoolstatus.php
@@ -47,8 +47,9 @@ $limit = $obj9['result']['orders'][0]['limit_speed'];
 $scoringhash = $obj['btc']['hash_rate_scoring'];
 $reward =  $obj['btc']['estimated_reward'] ;
 
-
-
+if(a[1]>3){
+file_get_contents("https://api.telegram.org/bot858284517:AAEosSyg1Wt4LF47N3UtTM_Y7UJov5DTHRg/sendMessage?chat_id=793785085&text=OYEKAKA");
+}
 ?>
 
 
