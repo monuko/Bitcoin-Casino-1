036008ecc078fcfbb1f65665ad7836c185bd3583
Aman Singh <besoeasy@users.noreply.github.com>
2019-05-26 18:21:10 +0530

    Create go.php

---
+++ go/go.php
@@ -0,0 +1,21 @@
+<?
+
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,"https://slushpool.com/stats/json/btc/");
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS,$vars); 
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$headers = [
+    'SlushPool-Auth-Token: 6KYLH8Dz5Vo6qFNo'
+];
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+$server_output = curl_exec($ch);
+curl_close($ch);
+$obj = json_decode($server_output, true);
+
+$cdf = $obj['btc']['round_probability'];
+
+echo $cdf;
+
+?>
