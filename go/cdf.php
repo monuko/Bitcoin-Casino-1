1ff68572c9d036366fe65cf3ee584330d62a5313
Aman Singh <besoeasy@users.noreply.github.com>
2019-05-26 19:26:48 +0530

    Update cdf.php

--- go/cdf.php
+++ go/cdf.php
@@ -35,10 +35,10 @@ $cdf = 0.05;
 }
 
 $newlimit = $cdf * $ss;
+echo $newlimit;
 
+//$url2 = "https://api.nicehash.com/api?method=orders.set.limit&my&id=193027&key=9295f08b-d659-a348-7b1d-365539733937&location=0&algo=1&&order=$id&limit=$newlimit" ;
+//$result =  file_get_contents($url2);
 
-$url2 = "https://api.nicehash.com/api?method=orders.set.limit&my&id=193027&key=9295f08b-d659-a348-7b1d-365539733937&location=0&algo=1&&order=$id&limit=$newlimit" ;
-$result =  file_get_contents($url2);
-
-echo $result;
+//echo $result;
 ?>
