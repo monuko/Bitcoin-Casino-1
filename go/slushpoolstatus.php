e1c1f28bcd36ac78492bb01813d14c9bf69e01d1
Aman Singh <besoeasy@users.noreply.github.com>
2019-05-26 17:34:19 +0530

    Update slushpoolstatus.php

--- go/slushpoolstatus.php
+++ go/slushpoolstatus.php
@@ -17,7 +17,7 @@ if(!strcmp($j['pool']['name'], "SlushPool")){
 $i = $i+1;
 $a[$i] = $j['confirmations'] ;
 
-echo "<br>". $a[$i] . " -> "  . ($a[$i] -$a[$i-1]) . " -> " . (($a[$i] + $a[$i-1]  + $a[$i-2]  + $a[$i-3])/4);
+echo "<br>". $a[$i] . " -> "  . ($a[$i] -$a[$i-1]) ;
 
 }
 }
